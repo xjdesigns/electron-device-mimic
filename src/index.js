a643bfbf38249705e9a352a5b07e8c294fc1351a
Jason Jacobson <jdesigns.jason@gmail.com>
2018-04-08 15:07:08 -0500

    worker, I have this set up to make connections and emit messages, catching from external app

---
+++ src/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var ipc = require('electron').ipcRenderer;
+var io = require('socket.io-client');
+var socket = io('http://localhost:7003');
+
+ipc.on('data-loaded', helloWorld)
+
+function helloWorld(event, data) {
+  console.warn('hello world func', data);
+}
+
+var btn = document.getElementById('clicker')
+console.warn('btn', btn);
+btn.onclick = simulateScan
+
+function simulateScan() {
+  ipc.send('simulate-scan');
+
+  var msg = 'I came from my simulator app';
+  socket.emit('device:sim', msg)
+}
+
+
+socket.on('device:sim', function(msg) {
+  console.warn('this works, I am INDEX JS');
+})
